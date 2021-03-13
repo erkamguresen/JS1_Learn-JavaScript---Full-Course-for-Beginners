c59cb64aa9f9209e068ee264faea80c6b17a428d
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-03-13 20:37:17 +0100

    progress update

---
+++ firstScript.js
@@ -0,0 +1,24 @@
+console.log("Hello World \t:-)");
+var number = 5; //inline comment
+/*
+this is comment
+*/
+var a;
+var b = 2;
+console.log("a:" + a + ", " + "b: " + b);
+
+a = 7;
+
+b = a;
+
+console.log("a:" + a + ", " + "b: " + b);
+
+a++;
+a += b;
+console.log("a:" + a + ", " + "b: " + b);
+
+let c = 3.4;
+
+let d = c * a;
+
+console.log("d: " + d);
