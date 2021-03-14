db5f3fa23f22c56709a0c597a862d00e66b1aee6
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-03-14 07:58:52 +0100

    function trials

---
+++ strings.js
@@ -0,0 +1,28 @@
+/**
+ * Make a sentence from the given words
+ * @param {string} myNoun
+ * @param {string} myAdjective
+ * @param {string} myVerb
+ * @param {string} myAdverb
+ * @returns a complete sentence build from the input parameters
+ */
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+
+  result +=
+    "The " +
+    myAdjective +
+    " " +
+    myNoun +
+    " " +
+    myVerb +
+    " " +
+    "to the store" +
+    " " +
+    myAdverb;
+
+  return result;
+}
+
+console.log(wordBlanks("dog", "big", "run", "quickly"));
+console.log(wordBlanks("dog", "big", 3, "quickly")); //interessant
