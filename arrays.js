adaa32c9df11618e8d13e9deecca08ef9475feeb
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-03-14 08:28:26 +0100

    update

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var ourArray = ["john", 23];
+
+console.log(ourArray);
+
+ourArray.push("new string to push");
+console.log(ourArray);
+
+var multiDimensionalArray = [
+  ["the universe", 42],
+  ["everthing", 101010],
+];
+console.log(multiDimensionalArray); //interessant
