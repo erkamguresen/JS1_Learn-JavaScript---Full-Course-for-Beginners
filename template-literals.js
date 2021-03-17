7b848abd59d2cea7f6f80dbdfe2524a697e8b7b6
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-03-17 08:02:50 +0100

    challeg completed

--- template-literals.js
+++ template-literals.js
@@ -7,8 +7,20 @@ I am ${person.age} years old.`;
 
 console.log(greetings);
 
-const result{
-    success: ['max-lenght', 'no-amd','prefer-arrow-functions'],
-    failure: ['no-var''var-on-top','line-break'],
-    skipped: ['id-blacklist','no-dup-keys']
+const result = {
+  success: ["max-lenght", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "line-break"],
+  skipped: ["id-blacklist", "no-dup-keys"],
 };
+
+function makeList(array) {
+  const resultDisplayArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    resultDisplayArray[i] = `<li class='text-warning'>${array[i]}</li>`;
+  }
+
+  return resultDisplayArray;
+}
+
+console.log(makeList(result.failure));
